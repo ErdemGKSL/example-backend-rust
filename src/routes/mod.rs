c0be1b66662aa4f113c4dc3a0e9f9a3ca101532a
Erdem <zahrakon06@gmail.com>
2023-12-15 23:22:18 +0300

    .

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,5 @@
 use axum::Router;
+use tower_http::cors:: CorsLayer;
 
 pub mod user;
 pub mod users;
@@ -7,4 +8,7 @@ pub fn get_router() -> Router {
   Router::new()
     .nest("/user", user::get_router())
     .nest("/users", users::get_router())
+    .layer(
+      CorsLayer::very_permissive()
+    )
 }
--- src/routes/user/id/get.rs
+++ src/routes/user/id/get.rs
@@ -5,7 +5,7 @@ use crate::library::user::get_users;
 
 pub async fn trigger(Path(user_id): Path<i32>) -> Json<Value> {
   let users = get_users();
-  let user = users.iter().find(|user| user.id == user_id);
+  let user = users.iter().find(|user| user.id == user_id ); // (user) => user.id == user_id
 
   if user.is_none() {
     return Json(json!({
