4c709ae65ae8ebc63d52ef6107442a88eef18366
Erdem <zahrakon06@gmail.com>
2023-12-16 19:57:48 +0300

    Update post.rs

--- src/routes/user/post.rs
+++ src/routes/user/post.rs
@@ -1,24 +1,23 @@
 use axum::extract::Json;
-use serde_json::Value;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 use crate::library::user::{add_user, get_users};
 
 pub async fn trigger(Json(user): Json<UserCreate>) -> Json<Value> {
-  add_user(user.name, user.email, user.password);
-  let user_opt = get_users().last();
+    add_user(user.name, user.email, user.password);
+    let user_opt = get_users().last();
 
-  if let Some(user) = user_opt {
-      Json(user.to_json())
-  } else {
-      unreachable!("User not found somehow, but added just now")
-  }
-
+    if let Some(user) = user_opt {
+        Json(user.to_json())
+    } else {
+        unreachable!("User not found somehow, but added just now")
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserCreate {
-  name: String,
-  email: String,
-  password: String,
+    name: String,
+    email: String,
+    password: String,
 }
