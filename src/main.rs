63b9d74b8746aa5813d265e65b2d12ca3417101d
Erdem <zahrakon06@gmail.com>
2024-01-07 13:43:54 +0300

    pre-integrated postgres db

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 mod routes;
 pub mod library;
 
+use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
+
 #[tokio::main]
 async fn main() {
-    let app = routes::get_router();
+    dotenv::dotenv().expect("Failed to read .env file");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env file");
+    let port = std::env::var("PORT").expect("PORT must be set in .env file");
+
+    let db: PgPool = PgPoolOptions::new()
+        .max_connections(20)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let app = routes::get_router(db);
     
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:9876").await.unwrap();
-    println!("Server started at {}", "http://localhost:9876");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:".to_owned() + port.as_str()).await.unwrap();
+    println!("Server started at http://localhost:{}", port);
 
     axum::serve(listener, app).await.unwrap();
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,14 @@
-use axum::Router;
+use axum::{Extension, Router};
+use sqlx::PgPool;
 use tower_http::cors:: CorsLayer;
 
 pub mod user;
 pub mod users;
 
-pub fn get_router() -> Router {
+pub fn get_router(db: PgPool) -> Router {
   Router::new()
+    .layer(Extension(db))
+    .layer(CorsLayer::very_permissive())
     .nest("/user", user::get_router())
     .nest("/users", users::get_router())
-    .layer(CorsLayer::very_permissive())
 }
